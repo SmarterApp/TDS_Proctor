a940df749efb6f92d396a80e560b4d7d1e02bd26
wtritch <wtritchler@fairwaytech.com>
2017-04-07 17:11:55 -0700

    Optimize imports.

--- proctor/src/main/java/TDS/Proctor/Services/ProctorWebConfiguration.java
+++ proctor/src/main/java/TDS/Proctor/Services/ProctorWebConfiguration.java
@@ -1,19 +1,8 @@
 package TDS.Proctor.Services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 import tds.shared.spring.configuration.WebConfiguration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @Import(WebConfiguration.class)
