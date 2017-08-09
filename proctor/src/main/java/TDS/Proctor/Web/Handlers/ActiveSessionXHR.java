55fde39872ed906a15b4650bf0089d20d4499a85
Michael Trupkin <mike.trupkin@gmail.com>
2017-08-09 09:25:43 -0700

    SessionDTO and other data structures were being sent as xml instead of json (#46)
    Explicitly returning json from enpoints

--- proctor/src/main/java/TDS/Proctor/Web/Handlers/ActiveSessionXHR.java
+++ proctor/src/main/java/TDS/Proctor/Web/Handlers/ActiveSessionXHR.java
@@ -48,6 +48,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -74,6 +75,7 @@ import static org.opentestsystem.delivery.logging.ProctorEventLogger.ProctorEven
 
 @Scope ("prototype")
 @Controller
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class ActiveSessionXHR extends HttpHandlerBase
 {
 private static final Logger _logger = LoggerFactory.getLogger(ActiveSessionXHR.class);
