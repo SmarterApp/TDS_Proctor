847e8be2f40d122e3054b8e79778ffab82739cab
Todd Miller <tmiller@fairwaytech.com>
2017-11-20 09:59:10 -0800

    Clean up handler

--- proctor/src/main/java/TDS/Proctor/Web/Handlers/ExamExpirationHandler.java
+++ proctor/src/main/java/TDS/Proctor/Web/Handlers/ExamExpirationHandler.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -37,7 +36,6 @@ public class ExamExpirationHandler {
      */
     @RequestMapping(value = "/exams/expire/{clientName}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 //    @Secured({ "ROLE_Assessment Modify" })
-    @ResponseBody
     ResponseEntity<List<ExpiredExamInformation>> expireExams(@PathVariable final String clientName) throws ReturnStatusException {
         return ResponseEntity.ok(examExpirationService.expireExams(clientName));
     }
